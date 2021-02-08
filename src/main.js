3121b5f77061626b7bb44189b5848a3b9cbe7f1d
Gabriel <gabrielcaiana@hotmail.com>
2021-02-07 22:39:51 -0300

    disponibilizando componente de icone globalmente na aplicação

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import BaseIcon from "@/components/BaseIcon";
+
+Vue.component("BaseIcon", BaseIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
